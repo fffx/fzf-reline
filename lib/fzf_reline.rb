2680c1ede9494a6c634cc0da81e3c0daecef1728
Fangxing <fangxing204@gmail.com>
2021-02-01 20:26:44 +0800

    Init

---
+++ lib/fzf_reline.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fzf_reline/version"
+
+module FzfReline
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/fzf_reline/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FzfReline
+  VERSION = "0.1.0"
+end
