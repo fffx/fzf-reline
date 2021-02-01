aebf6a76d2169ae1418167cf057767ae729e0c8e
Fangxing <fangxing204@gmail.com>
2021-02-01 21:47:21 +0800

    Overwrite reline private method

--- lib/fzf_reline.rb
+++ lib/fzf_reline.rb
@@ -6,3 +6,16 @@ module FzfReline
   class Error < StandardError; end
   # Your code goes here...
 end
+
+
+class Reline::LineEditor
+  private def incremental_search_history(key)
+    history_file = if $0 =~ /irb/i
+                     IRB.conf[:HISTORY_FILE]
+                   elsif $0 =~ /pry/i
+                     Pry.config.history_file
+                   end
+
+    Reline.insert_text  `cat #{history_file} | fzf --tac |  tr '\n' ' '`
+  end
+end
